d12d85aa8b2d9d748c937981159bf95fdda46994
sxxk2 <sxxklee@gmail.com>
2023-03-09 07:33:57 +0900

    feat: 로그인 (#11)

--- apps/user/urls.py
+++ apps/user/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 
-from apps.user.views import SignUpView
+from apps.user.views import LoginView, SignUpView
 
 app_name = "user"
 
-urlpatterns = [path("/signup", SignUpView.as_view(), name="user_signup")]
+urlpatterns = [
+    path("/signup", SignUpView.as_view(), name="user_signup"),
+    path("/login", LoginView.as_view(), name="user_login"),
+]
--- apps/user/views.py
+++ apps/user/views.py
@@ -3,7 +3,7 @@ from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
-from apps.user.serializers import SignUpSerializer
+from apps.user.serializers import LoginSerializer, SignUpSerializer
 
 
 # api/users/signup
@@ -19,6 +19,29 @@ class SignUpView(APIView):
             return Response(
                 {"message": "회원가입에 성공했습니다."},
                 status=status.HTTP_201_CREATED,
+            )
+        return Response(
+            serializer.errors,
+            status=status.HTTP_400_BAD_REQUEST,
+        )
+
+
+# api/users/login
+class LoginView(APIView):
+    permission_classes = [AllowAny]
+
+    def post(self, request):
+        serializer = LoginSerializer(data=request.data)
+
+        if serializer.is_valid():
+            token = serializer.validated_data
+            return Response(
+                {
+                    "message": "로그인 되었습니다.",
+                    "access_token": token["access"],
+                    "refresh_token": token["refresh"],
+                },
+                status=status.HTTP_200_OK,
             )
         return Response(
             serializer.errors,
