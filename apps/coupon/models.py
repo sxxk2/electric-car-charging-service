4b28849d47b6d1a05f36ab764b8150bbb8949adc
sxxk2 <sxxklee@gmail.com>
2023-03-10 11:55:27 +0900

    add: coupon 모델링 (#19)

--- apps/coupon/models.py
+++ apps/coupon/models.py
@@ -1,1 +1,19 @@
-# from django.db import models
+from django.contrib.auth.models import User
+from django.db import models
+from django.utils import timezone
+
+
+class Coupon(models.Model):
+    issuer = models.ForeignKey(User, on_delete=models.CASCADE)
+    code = models.CharField(max_length=15)
+    balance = models.PositiveIntegerField()
+    used_by = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True, related_name="used_coupons")
+    used_at = models.DateTimeField(null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    expired_at = models.DateTimeField()
+
+    def is_available(self):
+        return self.used_at is None and timezone.now() < self.expired_at
+
+    class Meta:
+        db_table = "coupons"
