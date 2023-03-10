1bc5f9d43f0ef3e7b6dfb73b90671a116f307b88
sxxk2 <sxxklee@gmail.com>
2023-03-10 16:36:09 +0900

    feat: 쿠폰 등록 (#23)

--- apps/coupon/models.py
+++ apps/coupon/models.py
@@ -1,15 +1,14 @@
-from django.conf import settings
 from django.db import models
 from django.utils import timezone
 
+from apps.user.models import User
+
 
 class Coupon(models.Model):
-    issuer = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    issuer = models.ForeignKey(User, on_delete=models.CASCADE)
     code = models.CharField(max_length=15)
     balance = models.PositiveIntegerField()
-    used_by = models.ForeignKey(
-        settings.AUTH_USER_MODEL, on_delete=models.CASCADE, null=True, blank=True, related_name="used_coupons"
-    )
+    used_by = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True, related_name="used_coupons")
     used_at = models.DateTimeField(null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     expired_at = models.DateTimeField()
