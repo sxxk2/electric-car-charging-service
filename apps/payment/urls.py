ab415f287e228c42f76864897925da8707841aa4
sxxk2 <sxxklee@gmail.com>
2023-03-11 04:33:39 +0900

    feat: 결제수단 상세조회, 수정, 삭제 (#41)

--- apps/payment/urls.py
+++ apps/payment/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from apps.payment.views import PaymentMethodView
+from apps.payment.views import PaymentMethodDetailView, PaymentMethodView
 
 app_name = "payment"
 
 urlpatterns = [
     path("payment-methods", PaymentMethodView.as_view(), name="payment_methods"),
+    path("payment-methods/<int:payment_method_id>", PaymentMethodDetailView.as_view(), name="payment_methods_detail"),
 ]
